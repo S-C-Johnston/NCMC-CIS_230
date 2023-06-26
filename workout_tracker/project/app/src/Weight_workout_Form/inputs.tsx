ab7899c0a5228ec0e56e5e87ecc3bfe9a22957f5
Stewart Johnston <johnstons1@student.ncmich.edu>
2023-06-25 20:52:23 -0400

    Add Weight_workout_Form/inputs component module
    Componentizing the Weight_workout_Form_inputs component into its own
    module file seems like the best way to proceed and keep things testable
    and small.
    
    I recognized that several inputs, not all, were largely boilerplate
    around the same ideas, and when I wanted to change something on one, I
    needed to change it on all. Perfect time to componentize

---
+++ workout_tracker/project/app/src/Weight_workout_Form/inputs.tsx
@@ -0,0 +1,42 @@
+import { Weight } from "../Weight"
+import { Weight_workout, Weight_workout_update_action } from "../Workout"
+
+export function Weight_workout_Form_inputs({
+    name_and_id,
+    type,
+    default_value,
+    label,
+    weight_workout_obj_field,
+    onchange_dispatch,
+    dispatch_type,
+    options
+}: {
+    name_and_id: string,
+    type: "number" | "text",
+    default_value: unknown,
+    label: string,
+    weight_workout_obj_field: any,
+    onchange_dispatch(dispatch_action: Weight_workout_update_action): void,
+    dispatch_type: Weight_workout_update_action["type"],
+    options?: {}
+}
+) {
+    return (
+        <>
+            <label
+                htmlFor={name_and_id}
+                className="form-label"
+            >{label ?? ""}</label>
+            <input
+                name={name_and_id}
+                id={name_and_id}
+                className="form-control"
+                type={type}
+                placeholder={default_value as string}
+                value={weight_workout_obj_field}
+                onChange={e => onchange_dispatch({ type: dispatch_type, value: e.target.value } as Weight_workout_update_action)}
+                {...options}
+            />
+        </>
+    )
+}
