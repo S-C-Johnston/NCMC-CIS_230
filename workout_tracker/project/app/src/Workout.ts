26f5bc46f9c4eb13dc4c01bff42c991aba433c02
Stewart Johnston <johnstons1@student.ncmich.edu>
2023-06-25 20:34:50 -0400

    Add pure reducer function for Weight_workout updates
    Prerequisite for replacing `useState()` with `useReducer()`.
    `useState()` is really more appropriate for a collection or a handful of
    individual, orthogonal states which can be set independently without
    affecting others. The whole state of Weight_workout is tightly
    interlinked within itself, firing off updates of total_weight, for
    instance, when any other property is set.
    
    `useState()` was being called inside update handlers linked to
    `onChange` properties, and itself supplied anonymous functions which
    dispatched the object's methods. This was a cumbersome mess, and created
    a high amount of coupling between the handler and the object.
    
    With `useReducer()`, I am able to supply react with the
    Weight_workout_reducer function, and it provides a dispatch function. In
    the Weight_workout_reducer function, I have defined a switch-case which
    handles all supported action types and their associated input values.
    This dramatically reduces coupling, requiring only type-system
    definition knowledge across modules/functions, rather than needing to
    know and implement gory details of calling the object's methods.

--- workout_tracker/project/app/src/Workout.ts
+++ workout_tracker/project/app/src/Workout.ts
@@ -5,6 +5,71 @@ interface Workout {
     exercise_name: string;
 }
 
+/** Weight_workout_update_action
+ * This discriminated union type is designed for use with the react
+ * `useReducer()` dispatch function, to define the acceptable input parameters.
+ * The inline union syntax `|` allows me to discriminate in the typesystem on
+ * the "type" of type to determine what type value should be. With the correct
+ * keywords, such as a type assertion, the typesystem will supply errors if the
+ * wrong or incomplete parameters are passed or assessed.
+ */
+export type Weight_workout_update_action = {
+    type: "exercise_name",
+    value: string
+} | {
+    type: "weight_unit",
+    value: WEIGHT_UNITS
+} | {
+    type: (
+        "sets"
+        | "repetitions"
+        | "weight_quantity"
+    ),
+    value: number
+}
+
+export function Weight_workout_reducer(
+    previous_Weight_workout: Weight_workout,
+    action: Weight_workout_update_action
+) {
+    let current_Weight_workout = new Weight_workout(previous_Weight_workout);
+    switch (action.type) {
+        case 'exercise_name': {
+            current_Weight_workout.exercise_name = action.value;
+            break;
+        }
+        case 'repetitions': {
+            current_Weight_workout.repetitions = action.value;
+            break;
+        }
+        case 'sets': {
+            current_Weight_workout.sets = action.value;
+            break;
+        }
+        case 'weight_quantity': {
+            current_Weight_workout.weight_quantity = action.value;
+            break;
+        }
+        case 'weight_unit': {
+            current_Weight_workout.weight_unit = action.value;
+            // The whole `as keyof typeof enum` trick is not
+            // necessary here, and it doesn't work anyway.
+            // keyof typeof is useful when the
+            // programmatically accessed .index is the value
+            // you're selecting. Here, the value is pulling
+            // from the Enum's values, so it'll just index
+            // off those fine. It's bizarre, I'm trying not
+            // to cross my eyes too hard about it.
+            break;
+        }
+        default: {
+            const exhaustive_check: never = action;
+            throw new Error(`Unhandled Weight_workout_update_action for type ${exhaustive_check}`);
+        }
+    }
+    return current_Weight_workout;
+}
+
 export class Weight_workout implements Workout {
     readonly id: Date;
     private my_sets: number;
