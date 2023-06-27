ccce4e5923341f7fe4b3bdfe343cbf19d5b5e31f
Stewart Johnston <johnstons1@student.ncmich.edu>
2023-06-26 23:18:54 -0400

    Add interface for workout db schema
    idb library is type aware and when an interface is defined for a
    database schema, it'll be aware of the kinds of things you can put in
    it.

--- workout_tracker/project/app/src/Workout_Database.ts
+++ workout_tracker/project/app/src/Workout_Database.ts
@@ -1,12 +1,31 @@
-import { openDB } from "idb";
+import { openDB, DBSchema } from "idb";
+import { nameof } from "./nameof";
+import { Weight_workout } from "./Workout";
+import { Weight } from "./Weight";
 
 const DATABASE_VERSION = 1;
-const DEFAULT_DATABASE_NAME = "workouts"
+const DEFAULT_DATABASE_NAME = "workouts_database";
+const DEFAULT_OBJECT_STORE_NAME = "workouts_store";
+const INDEX_STRING = nameof<Weight_workout>("exercise_name");
 
-export async function initialize_Workout_Database(db_name: string = DEFAULT_DATABASE_NAME) {
-    const database = openDB(db_name, DATABASE_VERSION,
+export interface workout_db_schema extends DBSchema {
+    workouts_store: {
+        key: Date;
+        value: Weight_workout;
+        indexes: any;
+        //Opting out of type-checking on this one. Can't set indexes
+        //programmatically because this is an interface, and it was screwing with my ability to set indices
+    }
+}
 
+export async function initialize_Workout_Database(
+    db_name: string = DEFAULT_DATABASE_NAME,
+    db_store_name: string = DEFAULT_OBJECT_STORE_NAME
+) {
+    const workout_database = await openDB<workout_db_schema>(db_name, DATABASE_VERSION,
     );
+
+    return workout_database;
 }
 
 // https://web.dev/learn/pwa/offline-data/#creating-and-opening-a-database
