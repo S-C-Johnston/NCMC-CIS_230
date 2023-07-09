1ea316f68a9a79e5be3cf0b61cafc935666779f8
Stewart Johnston <johnstons1@student.ncmich.edu>
2023-07-08 23:36:36 -0400

    Prototype Quiz interface in App
    Should refactor out later, but for now this is at least working.
    Probably should write a test next.

--- quiz_app/react/app/src/App.tsx
+++ quiz_app/react/app/src/App.tsx
@@ -1,10 +1,39 @@
 import React from 'react';
 import './App.css';
+import { Quiz } from "./quiz/quiz_data";
+import general_knowledge from "./general_knowledge_quiz.json";
 
 function App() {
 
+  let my_quiz: Quiz = general_knowledge;
+  console.log(`loaded Quiz: ${Object.values(my_quiz)}`);
+
   return (
     <>
+    <ul>
+        {my_quiz.questions.map(question => (
+          <li key={crypto.randomUUID()}>
+            {question.question}
+            <form action="">
+              <fieldset>
+                {question.answers.map(answer => (
+                  <>
+                    <input
+                      type="radio"
+                      name={question.question}
+                      id={question.answers.indexOf(answer).toString()}
+                      value={question.answers.indexOf(answer).toString()}
+                    />
+                    <label htmlFor={question.answers.indexOf(answer).toString()}>
+                      {answer}</label>
+                  </>
+                ))}
+              </fieldset>
+            </form>
+            {question.correct_answer_index}
+          </li>
+        ))}
+    </ul>
     </>
   );
 }
