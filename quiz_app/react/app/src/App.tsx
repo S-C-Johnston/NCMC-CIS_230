763b6b96f25f47dbe76e7d2769be4588d8752639
Stewart Johnston <johnstons1@student.ncmich.edu>
2023-07-14 18:52:16 -0400

    Update indentation formatting on App.tsx
    I don't know how the indentation spacing got out of sync with the rest
    of the project, but it was bugging me. It was set to 2 spaces instead of
    4 spaces. Ultimately I prefer tabs, but I'm trying to remain within the
    defaults as much as possible.

--- quiz_app/react/app/src/App.tsx
+++ quiz_app/react/app/src/App.tsx
@@ -8,101 +8,101 @@ import * as IDB from "idb"; // because I'm lazy
 import { initialize_Quiz_score_db, quiz_score_db_schema } from './database/score_tracking_database';
 
 function App() {
-  let my_quiz: Quiz = general_knowledge;
-  console.log(`loaded Quiz: ${Object.values(my_quiz)}`);
+    let my_quiz: Quiz = general_knowledge;
+    console.log(`loaded Quiz: ${Object.values(my_quiz)}`);
 
-  const quiz_scores_db_handle = useRef<IDB.IDBPDatabase<quiz_score_db_schema> | null>(null);
-  const [database_attached, set_database_attached] = useState<boolean>(false)
-  useEffect(() => {
-    (async () => {
-      quiz_scores_db_handle.current = await initialize_Quiz_score_db()
-      console.log(`Database attached!`);
-      set_database_attached(true);
-    })()
-  }, [])
+    const quiz_scores_db_handle = useRef<IDB.IDBPDatabase<quiz_score_db_schema> | null>(null);
+    const [database_attached, set_database_attached] = useState<boolean>(false)
+    useEffect(() => {
+        (async () => {
+            quiz_scores_db_handle.current = await initialize_Quiz_score_db()
+            console.log(`Database attached!`);
+            set_database_attached(true);
+        })()
+    }, [])
 
-  const [is_scored, set_is_scored] = useState(false);
-  const [final_score, set_final_score] = useState<string>();
-  const handle_score_click = useCallback(() => {
-    set_final_score(score_Quiz(my_quiz, selected_answers));
-    set_is_scored(true);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    const [is_scored, set_is_scored] = useState(false);
+    const [final_score, set_final_score] = useState<string>();
+    const handle_score_click = useCallback(() => {
+        set_final_score(score_Quiz(my_quiz, selected_answers));
+        set_is_scored(true);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
-  /**  This whole callback is invoked every time one of the members of the
-  *  dependency array changes. The invocation guard just avoids that whole
-  *  shebang until the conditions we need to be true, are. This technique is
-  *  a little clunkier than I want, but absolutely avoids any weird async
-  *  race conditions that I was experiencing earlier. I *wanted* all this to
-  *  be part of the handle_score_click callback defined above, but there was
-  *  just no way to guarantee that the callback would be fired, as an event
-  *  handler for user input, when and only when the database was attached. This
-  *  will fire, guaranteed, after both is_scored is changed, and after the
-  *  database handle is attached.
-  */
-  useEffect(() => {
-    if (!(database_attached && is_scored)) {
-      // console.log(`database attached: ${database_attached} && is_scored: ${is_scored}`);
-      return;
-    };
-    console.log(`database attached, should get transaction!`);
-    const tx = quiz_scores_db_handle.current!.transaction("quiz_scores_store", "readwrite");
-    console.log(`About to add Quiz score to idb!`);
-    tx.store.put({
-      Date: new Date(),
-      quiz_name: my_quiz.name,
-      topic: my_quiz.topic,
-      percentage_correct: final_score!
-    })
-      .then(response => {
-        console.log(`Added Quiz score to idb! ${response}`)
-      })
-      .catch(error => {
-        console.log(`Something when wrong trying to add Quiz score to the idb!`)
-        console.log(`${error}`);
-      })
-      .finally(() => console.log(`A database operation happened and finished`));
-  },
-    [
-      database_attached,
-      final_score,
-      my_quiz,
-      is_scored
-    ]);
+    /**  This whole callback is invoked every time one of the members of the
+    *  dependency array changes. The invocation guard just avoids that whole
+    *  shebang until the conditions we need to be true, are. This technique is
+    *  a little clunkier than I want, but absolutely avoids any weird async
+    *  race conditions that I was experiencing earlier. I *wanted* all this to
+    *  be part of the handle_score_click callback defined above, but there was
+    *  just no way to guarantee that the callback would be fired, as an event
+    *  handler for user input, when and only when the database was attached. This
+    *  will fire, guaranteed, after both is_scored is changed, and after the
+    *  database handle is attached.
+    */
+    useEffect(() => {
+        if (!(database_attached && is_scored)) {
+            // console.log(`database attached: ${database_attached} && is_scored: ${is_scored}`);
+            return;
+        };
+        console.log(`database attached, should get transaction!`);
+        const tx = quiz_scores_db_handle.current!.transaction("quiz_scores_store", "readwrite");
+        console.log(`About to add Quiz score to idb!`);
+        tx.store.put({
+            Date: new Date(),
+            quiz_name: my_quiz.name,
+            topic: my_quiz.topic,
+            percentage_correct: final_score!
+        })
+            .then(response => {
+                console.log(`Added Quiz score to idb! ${response}`)
+            })
+            .catch(error => {
+                console.log(`Something when wrong trying to add Quiz score to the idb!`)
+                console.log(`${error}`);
+            })
+            .finally(() => console.log(`A database operation happened and finished`));
+    },
+        [
+            database_attached,
+            final_score,
+            my_quiz,
+            is_scored
+        ]);
 
-  const [selected_answers, select_answer] = useState<Map<string, number>>(new Map());
-  const handle_radio_click = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    select_answer(
-      selected_answers =>
-        selected_answers.set(e.target.name, Number(e.target.value))
+    const [selected_answers, select_answer] = useState<Map<string, number>>(new Map());
+    const handle_radio_click = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+        select_answer(
+            selected_answers =>
+                selected_answers.set(e.target.name, Number(e.target.value))
+        );
+        console.log(selected_answers);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    return (
+        <section>
+            <h1 className="App">{my_quiz.name}</h1>
+            <h2 className="App">{my_quiz.topic}</h2>
+            {my_quiz.questions.map(question => (
+                <Quiz_Question_Form
+                    question={question}
+                    radio_callback={handle_radio_click}
+                    is_scored={is_scored}
+                    key={question.question}
+                />
+            ))}
+            <button
+                className="Score-button"
+                type="button"
+                onClick={handle_score_click}
+                disabled={is_scored}
+            >
+                SCORE!
+            </button>
+            <h3 className="App">Score: {is_scored ? final_score : "?"}</h3>
+        </section>
     );
-    console.log(selected_answers);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  return (
-    <section>
-      <h1 className="App">{my_quiz.name}</h1>
-      <h2 className="App">{my_quiz.topic}</h2>
-      {my_quiz.questions.map(question => (
-        <Quiz_Question_Form
-          question={question}
-          radio_callback={handle_radio_click}
-          is_scored={is_scored}
-          key={question.question}
-        />
-      ))}
-        <button
-          className="Score-button"
-          type="button"
-          onClick={handle_score_click}
-          disabled={is_scored}
-        >
-          SCORE!
-        </button>
-      <h3 className="App">Score: {is_scored ? final_score : "?"}</h3>
-    </section>
-  );
 };
 
 export default App;
