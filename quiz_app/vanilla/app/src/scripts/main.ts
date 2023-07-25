20351e6f51257cb08f7d3bcce396541b87c7420c
Stewart Johnston <johnstons1@student.ncmich.edu>
2023-07-24 21:05:10 -0400

    Update Main to use a class oriented approach
    Ironically I think I was causing myself an unexpected problem by doing
    this. idk. I did not know about the issue with binding "this", and I
    have a new distaste for javascript. I'd rather that be an opt-in feature
    than opt-out, which is very clunky. Got stuck on that for a while.

--- quiz_app/vanilla/app/src/scripts/main.ts
+++ quiz_app/vanilla/app/src/scripts/main.ts
@@ -2,10 +2,25 @@ import Model from "./model.js";
 import { CUSTOM_EVENTS } from "./types.js";
 import View from "./view.js";
 
-export default function main() {
-    const _model = new Model();
-    const _view = new View(_model.state);
-    _view.render();
+export default class main {
+    _model: Model;
+    _view: View;
+    constructor() {
+        this._model = new Model({});
+        this.update_callback = this.update_callback.bind(this);
+        //Binding the callback to "this" is necessary because passing the
+        //function to _view allows its "this" context to take on the context
+        //from which it is called.
+        this._view = new View(this._model.state, this.update_callback);
+        this._view.render();
+    };
+
+    update_callback(e: Event){
+        const input = e.target as HTMLInputElement;
+        console.log(`Received event ${e} of type ${e.type}`);
+        console.log(input.name, input.value)
+        this._model.add_selected_answer(input.name, Number(input.value));
+    };
 };
 
-main();
+new main();
