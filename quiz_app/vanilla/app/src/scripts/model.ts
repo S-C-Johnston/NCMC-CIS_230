8fd921c83924be7402cb28f2b154bd913b8f9dad
Stewart Johnston <johnstons1@student.ncmich.edu>
2023-07-24 20:52:06 -0400

    Flesh out Model class

--- quiz_app/vanilla/app/src/scripts/model.ts
+++ quiz_app/vanilla/app/src/scripts/model.ts
@@ -1,11 +1,30 @@
-import { Quiz } from "./quiz/quiz_data.js";
+import { Quiz, Quiz_Question, Quiz_score } from "./quiz/quiz_data.js";
 import { general_knowledge } from "./general_knowledge_quiz.js";
 
+export type model_state = {
+    quiz: Quiz;
+    is_scored?: boolean;
+    score?: Quiz_score;
+    selected_answers?: Map<string, number>;
+};
+
 export default class Model {
-    state: Quiz;
-    constructor(
-        state: Quiz = general_knowledge as Quiz
-    ) {
-        this.state = state;
+    state: model_state;
+    constructor({
+        quiz = general_knowledge as Quiz
+    }: {
+        quiz?: Quiz;
+    }) {
+        this.state = { quiz };
+        this.state.is_scored = false;
+        this.state.selected_answers = new Map<string, number>(null);
     };
+
+    add_selected_answer(question: string, answer_index: number) {
+        console.log("About to add to selected_answers");
+        this.state.selected_answers!.set(question, answer_index);
+        console.log("Added: ", this.state.selected_answers!.get(question));
+        return this.state;
+    };
+
 };
