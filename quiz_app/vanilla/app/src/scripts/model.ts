90b84c81364f532ac67b79b6250098f2d11d6cc2
Stewart Johnston <johnstons1@student.ncmich.edu>
2023-07-28 21:01:57 -0600

    Add database handle attachment to Model
    The import of idb is resolved completely down to the js file because of
    the ES native module handling.
    
    Not seen around the db_handle_ready property is the troubleshooting
    performed around the problem of defining a part of the object
    asynchronously. I need to `await` the database connection if I want to
    assign it in the constructor, and I can't really make the constructor
    async... An async IIFE doesn't work to satisfy the typescript compiler
    that the property is definitely assigned in the constructor, and I don't
    blame it. The constructor needs to return an object of the class, with a
    known state.  A promise is in some sense a known state, for a number of
    reasons. And, per the SO post, I can use the Promise as effectively
    equivalent to a handle, defining what I want to do in a `.then()` call.
    
    This does help avoid some weirdness around async/await functions, since
    I can define a `.then()` and keep moving right along without needing to
    break a function up into all the crap that happens before an `await` and
    all the stuff that depends on that `await`.

--- quiz_app/vanilla/app/src/scripts/model.ts
+++ quiz_app/vanilla/app/src/scripts/model.ts
@@ -1,5 +1,7 @@
 import { Quiz, Quiz_Question, Quiz_score } from "./quiz/quiz_data.js";
 import { general_knowledge } from "./general_knowledge_quiz.js";
+import { QUIZ_SCORE_DB_INDICES, initialize_Quiz_score_db, quiz_score_db_schema } from "./database/score_tracking_database.js";
+import { IDBPDatabase } from "../../node_modules/idb/build/index.js";
 
 export type model_state = {
     quiz: Quiz;
@@ -10,6 +12,11 @@ export type model_state = {
 
 export default class Model {
     state: model_state;
+    quiz_scores_db_handle_ready: Promise<IDBPDatabase<quiz_score_db_schema>>;
+    // https://stackoverflow.com/a/45070748
+    // Given that the model inherently relies on the availability of the
+    // database handle, this pattern works great.
+
     constructor({
         quiz = general_knowledge as Quiz
     }: {
@@ -18,6 +25,11 @@ export default class Model {
         this.state = { quiz };
         this.state.is_scored = false;
         this.state.selected_answers = new Map<string, number>(null);
+
+        this.quiz_scores_db_handle_ready = initialize_Quiz_score_db();
+        this.quiz_scores_db_handle_ready
+            .then(result => console.log(`Database handle attached! ${result}`))
+            .catch(error => console.log(`Some error occurred! ${error}`));
     };
 
     add_selected_answer(question: string, answer_index: number) {
